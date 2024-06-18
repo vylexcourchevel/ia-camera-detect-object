70b4db144fe217906af837241e481e0252f39581
Marianna Demchenko <mdemko2012@gmail.comm>
2024-06-18 09:24:22 +0200

    db added1

--- src/db.js
+++ src/db.js
@@ -1,35 +1,3 @@
-// import { openDB } from 'idb';
-
-// const dbPromise = openDB('my-database', 1, {
-//   upgrade(db) {
-//     if (!db.objectStoreNames.contains('images')) {
-//       db.createObjectStore('images', { keyPath: 'id', autoIncrement: true });
-//     }
-//   },
-// });
-
-// export const addImage = async (image) => {
-//   const db = await dbPromise;
-//   const tx = db.transaction('images', 'readwrite');
-//   const store = tx.objectStore('images');
-//   await store.add(image);
-//   await tx.done;
-// };
-
-// export const getImages = async () => {
-//   const db = await dbPromise;
-//   const tx = db.transaction('images', 'readonly');
-//   const store = tx.objectStore('images');
-//   return await store.getAll();
-// };
-
-// export const clearImages = async () => {
-//   const db = await dbPromise;
-//   const tx = db.transaction('images', 'readwrite');
-//   const store = tx.objectStore('images');
-//   await store.clear();
-//   await tx.done;
-// };
 import { openDB } from 'idb';
 
 const dbPromise = openDB('my-database', 1, {
